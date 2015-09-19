420218524cbccc9f7b7e5c9b79410ebe14e50374
sezalcruz <cesarcruz91@gmail.com>
2015-09-19 18:47:01 -0400

    integrated the static content to angular application

--- app/app.js
+++ app/app.js
@@ -3,8 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2'
+  'myApp.view1'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
