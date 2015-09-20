48d16bf6c5afbb31f215325522605e9b61ae93c5
sezalcruz <cesarcruz91@gmail.com>
2015-09-20 09:22:30 -0400

    Styling fixes and added more heatmaps

--- app/mapComponent/mapController.js
+++ app/mapComponent/mapController.js
@@ -10,33 +10,25 @@ angular.module('myApp.mapViewController', ['ngRoute', 'ngMap', 'angularAwesomeSl
 
 .controller('mapController', function($scope, $http) {
 
+	//Variables
 	$scope.ready = false;
-
 	var time = new Date();
-
 	var hour = time.getHours();
-
 	var adjustedTime = hour - (hour % 3)
-
 	time.setHours(adjustedTime);
-
 	time.setMinutes(0);
-
 	time.setSeconds(0);
-
-	$scope.sliderValue = 20;
-
+	$scope.sliderValue = 0;
 	$scope.sliderOptions = {				
-        from: 1,
-        to: 100,
+        from: 12,
+        to: 0,
         floor: true,
-        step: 1,
-        dimension: " km",
+        step: 6,
+        dimension: "Hrs",
         vertical: false,
         css: {
           background: {"background-color": "silver"},
-          before: {"background-color": "purple"},
-          default: {"background-color": "white"},
+          before: {"background-color": "white"},
           after: {"background-color": "green"},
           pointer: {"background-color": "red"}          
         },    
@@ -45,18 +37,45 @@ angular.module('myApp.mapViewController', ['ngRoute', 'ngMap', 'angularAwesomeSl
         }				
     };
 
+    var blueGradient = [
+      'rgba(0, 255, 255, 0)',
+      'rgba(0, 255, 255, 1)',
+      'rgba(0, 191, 255, 1)',
+      'rgba(0, 127, 255, 1)',
+      'rgba(0, 63, 255, 1)',
+      'rgba(0, 0, 255, 1)',
+      'rgba(0, 0, 223, 1)',
+      'rgba(0, 0, 191, 1)',
+      'rgba(0, 0, 159, 1)',
+      'rgba(0, 0, 127, 1)',
+      'rgba(63, 0, 91, 1)',
+      'rgba(127, 0, 63, 1)',
+      'rgba(191, 0, 31, 1)',
+      'rgba(255, 0, 0, 1)'
+    ];
+
+    var yellowGradient = [
+      'rgba(0, 255, 255, 0)',
+      'rgba(0, 255, 255, 1)',
+      'rgba(0, 191, 255, 1)',
+      'rgba(0, 127, 255, 1)',
+      'rgba(0, 63, 255, 1)',
+      'rgba(0, 0, 255, 1)',
+      'rgba(0, 0, 223, 1)',
+      'rgba(0, 0, 191, 1)',
+      'rgba(0, 0, 159, 1)',
+      'rgba(0, 0, 127, 1)',
+      'rgba(63, 0, 91, 1)',
+      'rgba(127, 0, 63, 1)',
+      'rgba(191, 0, 31, 1)',
+      'rgba(255, 0, 0, 1)'
+    ]
+
     var negativeData = [];
-
 	var positiveData = [];
-
 	var neutralData = [];
-
-	var final = [];
-
 	$scope.negativeData = [];
-
 	$scope.positiveData = [];
-
 	$scope.neutralData = [];
 
 	$http({
@@ -94,7 +113,9 @@ angular.module('myApp.mapViewController', ['ngRoute', 'ngMap', 'angularAwesomeSl
 			$scope.neutralData.push(new google.maps.LatLng(location[1], location[0]));
 		});
 
-		//Wait until data is ready to setup map
+
+
+		//Wait until data is ready to setup maps
 		$scope.ready = true;
 
 
@@ -105,34 +126,19 @@ angular.module('myApp.mapViewController', ['ngRoute', 'ngMap', 'angularAwesomeSl
 		console.log(err);
 	});
 
-	var heatmap;
+	var positiveHeatmap, negativeHeatmap, neutralHeatmap;
 	$scope.$on('mapInitialized', function(event, map){
-		heatmap = map.heatmapLayers.foo;
+		positiveHeatmap = map.heatmapLayers.positive;
+		negativeHeatmap = map.heatmapLayers.negative;
+		neutralHeatmap = map.heatmapLayers.neutral;
+		positiveHeatmap.set('gradient', heatmap.get('gradient') ? null : blueGradient);
+		negativeHeatmap.set('gradient', heatmap.get('gradient') ? null : redGradient);
+		neutralHeatmap.set('gradient', heatmap.get('gradient') ? null : yellowGradient);
+
 	});
 
-	$scope.toggleHeatmap= function(event) {
-    	heatmap.setMap(heatmap.getMap() ? null : $scope.map);
-  	};
-
   
-  $scope.changeGradient = function() {
-    var gradient = [
-      'rgba(0, 255, 255, 0)',
-      'rgba(0, 255, 255, 1)',
-      'rgba(0, 191, 255, 1)',
-      'rgba(0, 127, 255, 1)',
-      'rgba(0, 63, 255, 1)',
-      'rgba(0, 0, 255, 1)',
-      'rgba(0, 0, 223, 1)',
-      'rgba(0, 0, 191, 1)',
-      'rgba(0, 0, 159, 1)',
-      'rgba(0, 0, 127, 1)',
-      'rgba(63, 0, 91, 1)',
-      'rgba(127, 0, 63, 1)',
-      'rgba(191, 0, 31, 1)',
-      'rgba(255, 0, 0, 1)'
-    ]
-    heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
-  }
+    
+   
    
 });
